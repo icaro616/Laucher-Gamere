fad2d9a90d91b80b8492aad3fc381b218b262350
icaro616 <85366905+icaro616@users.noreply.github.com>
2023-05-18 15:39:58 -0300

    Beta v1
    um lancher game

---
+++ Pacote de games v1.0/Pacote de games v1.0/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Pacote_de_games_v1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Jogos_v1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Jogos_v1
+            // 
+            this.Jogos_v1.Font = new System.Drawing.Font("Cambria Math", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Jogos_v1.FormattingEnabled = true;
+            this.Jogos_v1.ItemHeight = 73;
+            this.Jogos_v1.Location = new System.Drawing.Point(52, 217);
+            this.Jogos_v1.Name = "Jogos_v1";
+            this.Jogos_v1.Size = new System.Drawing.Size(427, 369);
+            this.Jogos_v1.TabIndex = 0;
+            this.Jogos_v1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(886, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(62, 55);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Pacote_de_games_v1._0.Properties.Resources.img13;
+            this.pictureBox1.Location = new System.Drawing.Point(659, 161);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(289, 372);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Image = global::Pacote_de_games_v1._0.Properties.Resources.close1;
+            this.button3.Location = new System.Drawing.Point(913, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 54);
+            this.button3.TabIndex = 4;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Image = global::Pacote_de_games_v1._0.Properties.Resources.bt;
+            this.button4.Location = new System.Drawing.Point(659, 539);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(289, 139);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // form1BindingSource
+            // 
+            this.form1BindingSource.DataSource = typeof(Pacote_de_games_v1._0.Form1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1000, 750);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Jogos_v1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximumSize = new System.Drawing.Size(1000, 750);
+            this.MinimumSize = new System.Drawing.Size(1000, 750);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pacote de games v1.0";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox Jogos_v1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.BindingSource form1BindingSource;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Pacote de games v1.0/Pacote de games v1.0/Form1.cs
@@ -0,0 +1,185 @@
+﻿using AventStack.ExtentReports.Gherkin.Model;
+using Pacote_de_games_v1._0.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+
+
+namespace Pacote_de_games_v1._0
+{
+    public partial class Form1 : Form
+    {
+        private readonly SecureString administrador;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            Jogos_v1.Items.Add("Assassin's Creed I");
+            Jogos_v1.Items.Add("Assassin's Creed II");
+            Jogos_v1.Items.Add("Battlefield 4");
+            Jogos_v1.Items.Add("Call of Duty - Black Ops");
+            Jogos_v1.Items.Add("Call of Duty Modern Warfare 3");
+            Jogos_v1.Items.Add("Devil May Cry 5");
+            Jogos_v1.Items.Add("Exo One");
+            Jogos_v1.Items.Add("Grand Theft Auto V");
+            Jogos_v1.Items.Add("Need for Speed Most Wanted Limited Edition");
+            Jogos_v1.Items.Add("Onimusha Warlords");
+            
+
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            var jogo1 = Resources.img1;
+            var jogo2 = Resources.img2;
+            var jogo3 = Resources.img3;
+            var jogo4 = Resources.img4;
+            var jogo5 = Resources.img5;
+            var jogo6 = Resources.img6;
+            var jogo7 = Resources.img7;
+            var jogo8 = Resources.img8;
+            var jogo9 = Resources.img9;
+            var jogo10 = Resources.img10;
+            var jogo11 = Resources.img11;
+
+           
+            switch (Jogos_v1.Text)
+            {
+                case "Assassin's Creed I":
+                    pictureBox1.Image = Properties.Resources.img1;
+                    break;
+                case "Assassin's Creed II":
+                    pictureBox1.Image = Properties.Resources.img2;
+                    break;
+                case "Battlefield 4":
+                    pictureBox1.Image = Properties.Resources.img3;
+                    break;
+                case "Call of Duty - Black Ops":
+                    pictureBox1.Image = Properties.Resources.img4;
+                    break;
+                case "Call of Duty Modern Warfare 3":
+                    pictureBox1.Image = Properties.Resources.img5;
+                    break;
+                case "Devil May Cry 5":
+                    pictureBox1.Image = Properties.Resources.img6;
+                    break;
+                case "Exo One":
+                    pictureBox1.Image = Properties.Resources.img7;
+                    break;
+                case "Grand Theft Auto V":
+                    pictureBox1.Image = Properties.Resources.img8;
+                    break;
+                case "Need for Speed Most Wanted Limited Edition":
+                    pictureBox1.Image = Properties.Resources.img9;
+                    break;
+                case "Onimusha Warlords":
+                    pictureBox1.Image = Properties.Resources.img10;
+                    break;
+               
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            Application.Exit();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+    
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            var jogo1 = Resources.img1;
+            var jogo2 = Resources.img2;
+            var jogo3 = Resources.img3;
+            var jogo4 = Resources.img4;
+            var jogo5 = Resources.img5;
+            var jogo6 = Resources.img6;
+            var jogo7 = Resources.img7;
+            var jogo8 = Resources.img8;
+            var jogo9 = Resources.img9;
+            var jogo10 = Resources.img10;
+            var jogo11 = Resources.img11;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            switch (Jogos_v1.Text)
+            {
+                case "Assassin's Creed I":
+                    System.Diagnostics.Process.Start("C:/Program Files (x86)/Ubisoft/Assassin's Creed/AssassinsCreed_Game.exe");
+                    Application.Exit();
+                    break;
+                case "Assassin's Creed II":
+                    System.Diagnostics.Process.Start("C:/Games/Assassins Creed II/AssassinsCreedIIGame.exe");
+                    Application.Exit();
+                    break;
+                case "Battlefield 4":
+                    System.Diagnostics.Process.Start("C:/Program Files (x86)/Battlefield 4/bf4_x86.exe");
+                    Application.Exit();
+                    break;
+                case "Call of Duty - Black Ops":
+                    System.Diagnostics.Process.Start("C:/Program Files (x86)/Activision/Call of Duty - Black Ops/game1.lnk");
+                    Application.Exit();
+                    break;
+                case "Call of Duty Modern Warfare 3":
+                    System.Diagnostics.Process.Start("C:/Program Files (x86)/Black_Box/Call of Duty Modern Warfare 3/gam2.lnk");
+                    Application.Exit();
+                    break;
+                case "Devil May Cry 5":
+                    System.Diagnostics.Process.Start("C:/Games/Devil May Cry 5/game3.lnk");
+                    Application.Exit();
+                    break;
+                case "Exo One":
+                    System.Diagnostics.Process.Start("C:/Program Files (x86)/Exo One/game4.lnk");
+                    Application.Exit();
+                    break;
+                case "Grand Theft Auto V":
+                    System.Diagnostics.Process.Start("C:/Games/Grand Theft Auto V/game5.lnk");
+                    Application.Exit();
+                    break;
+                case "Need for Speed Most Wanted Limited Edition":
+                    System.Diagnostics.Process.Start("C:/Program Files (x86)/Need for Speed Most Wanted Limited Edition/game7.lnk");
+                    Application.Exit();
+                    break;
+                case "Onimusha Warlords":
+                    System.Diagnostics.Process.Start("C:/Program Files (x86)/Onimusha Warlords/game8.lnk");
+                    Application.Exit();
+                    break;
+              
+            }
+
+        }
+    }
+}
